ad9622ec10582242f3c6cda790046e21126208d8
Patrick <plmiranda@rdretailgroup.com.ph>
2024-11-28 09:36:41 +0800

    minor changes on sign-up

--- app/auth/sign-up/components/register-member.tsx
+++ app/auth/sign-up/components/register-member.tsx
@@ -18,7 +18,8 @@ import {
 } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 import { memberRegister } from "@/actions/queries";
-import ConfirmationModal from "@/app/confirmation/page";
+import ConfirmationModal from "./modal-confirmation";
+
 
 
 export const MemberRegisterForm = () => {
