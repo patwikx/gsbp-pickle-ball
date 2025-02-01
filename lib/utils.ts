ac56100d7a4249075c769a916a6e8e63e3a3b379
Patrick <plmiranda@rdretailgroup.com.ph>
2025-02-01 11:14:11 +0800

    added ignore eslint error on QR utisl

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 import { customAlphabet } from 'nanoid';
