dcf7e99f97fd5aebc8e63884a18c7d9b18ecfc7e
Patrick <plmiranda@rdretailgroup.com.ph>
2025-02-18 13:37:41 +0800

    removed pop ups on dashboard home

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,6 @@
 
-
-import { AdPopup1 } from '@/components/ad-pop-up1'
 import UserBookingsNew from './components/dashboard-page'
-import { AdPopup2 } from '@/components/ad-pop-up2'
+
 
 
 export default function HomeDashboardPage() {
@@ -10,8 +8,6 @@ export default function HomeDashboardPage() {
     <div className="min-h-screen bg-gradient-to-b from-gray-50 to-gray-100 px-4">
 
 <UserBookingsNew />
-<AdPopup1 />
-<AdPopup2  />
     </div>
   )
 }
